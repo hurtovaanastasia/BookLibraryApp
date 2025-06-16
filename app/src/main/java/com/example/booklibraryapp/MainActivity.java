b94b605dc1e7f4cb7a43273adac3f06132c2fc3b
Анастасия <hurtovanasta85@gmail.com>
2025-06-16 11:27:48 +0300

    коммит

--- app/src/main/java/com/example/booklibraryapp/MainActivity.java
+++ app/src/main/java/com/example/booklibraryapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
